e1558129495407c5c2a8fe705d044ef62636d856
Lukas Boehm <lukas.l.boehm@fau.de>
2025-03-26 21:00:00 +0100

    aaaa

--- model/ball_tracker.py
+++ model/ball_tracker.py
@@ -42,7 +42,7 @@ kalman.errorCovPost = np.eye(6, dtype=np.float32)
 
 # initialize background subtractor
 background = cv2.createBackgroundSubtractorKNN(
-    history=400, dist2Threshold=200, detectShadows=False
+    history=200, dist2Threshold=200, detectShadows=False
 )
 
 # History for trail visualization
@@ -58,47 +58,48 @@ while cap.isOpened():
     i += 1
 
     # preprocess
-    frame = cv2.resize(frame, (640, 480))
+    frame = cv2.resize(frame, (720, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
     mask = background.apply(blur)
-    _, thresh = cv2.threshold(mask, 90, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(mask, 20, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((2, 2), np.uint8)
+    erode = cv2.erode(thresh, kernel, iterations=1)
+    dilated = cv2.dilate(erode, kernel, iterations=4)
+    processed = dilated
 
-    if i < 400:
+    if i < 2500:
         continue
 
     # find contours
-    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # find the smallest blob (ball)
     ball_detected = False
     if contours:
-        contours = [c for c in contours if cv2.contourArea(c) > 5]
-        sort = sorted(contours, key=cv2.contourArea, reverse=False)
+        # filter out noise and sort
+        small_contours = [c for c in contours if 1 < cv2.contourArea(c) < 100]
+        big_contours = [c for c in contours if cv2.contourArea(c) > 1000]
 
-        player1 = None
-        player2 = None
+        print(f"detected {len(small_contours)} ball sized contours in frame {i}")
+        print(f"detected {len(big_contours)} player sized contours in frame {i}")
 
-        for c in contours:
-            # players
-            if cv2.contourArea(c) > 1000:
-                cv2.drawContours(frame, [c], -1, (0, 0, 255), 4)
-                (x, y), radius = cv2.minEnclosingCircle(c)
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        for c in big_contours:
+            cv2.drawContours(frame, [c], -1, (0, 0, 255), 4)
+            # (x, y), radius = cv2.minEnclosingCircle(c)
+            # cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 
-        # print(f"detected {len(contours)} object")
-        # for c in contours:
-        #     (x, y), radius = cv2.minEnclosingCircle(c)
-        #     if cv2.pointPolygonTest(player1, (int(x), int(y)), False) > 0:
-        #         continue
-        #     if cv2.pointPolygonTest(player2, (int(x), int(y)), False) > 0:
-        #         continue
-        #     cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-        # contour is likely the ball if it is outside the player circle!
-        # the players are the two biggest circles!
+        for c in small_contours:
+            (x, y), radius = cv2.minEnclosingCircle(c)
+            outside_of_player = [cv2.pointPolygonTest(player, (int(x), int(y)), False) for player in big_contours]
+            print(outside_of_player)
+            # if any(outside_of_player):
+            #     ball_detected = True
+            #     cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            #     break
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("Mask + Threash", thresh)
+    cv2.imshow("After PreProcessing", processed)
     if cv2.waitKey(1000 // int(fps)) & 0xFF == ord('q'):
         break
     # FIXME: remove
