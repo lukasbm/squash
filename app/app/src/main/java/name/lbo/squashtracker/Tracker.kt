e76838fc72646e1609299a303fc25da456f2e49d
Lukas Boehm <lukas.l.boehm@fau.de>
2025-04-27 02:11:49 +0200

    im tired

--- app/app/src/main/java/name/lbo/squashtracker/Tracker.kt
+++ app/app/src/main/java/name/lbo/squashtracker/Tracker.kt
@@ -6,6 +6,7 @@ import org.opencv.core.Rect
 import org.opencv.imgproc.Imgproc
 
 class Tracker() {
+    // FIXME: is this correctly updated?
     private val candidateHistory = CircularBuffer<Collection<Rect>>(7)
 
     private val avgArea = 25 * 24
@@ -20,25 +21,39 @@ class Tracker() {
         candidateHistory.add(setOf(dummyCandidate))
     }
 
-    // Finds the shortest path through sequences of ball candidates.
-    private fun shortestPathCandidate()
+    public fun selectMostPromisingCandidate(frame: Mat, prediction: Rect): Rect {
+        val candidates = findBallCandidates(frame).ifEmpty { mutableListOf(prediction) }
+        val bestCandidate = findShortestPathCandidate(candidates, prediction)
+        return bestCandidate
+    }
+
+    private fun findBallCandidates(frame: Mat): Collection<Rect> {
+        val contours = getContours(frame)
+        val ballCandidates = contours.filter { avgArea * 0.3 < it.area() && it.area() < avgArea * 3 }
+        return ballCandidates.sortedBy { it.area() }.dropLast(2) // drop players
+    }
+
+    private fun findShortestPathCandidate(candidates: Collection<Rect>, prediction: Rect): Rect {
+
+    }
 
     private fun joinNearbyBoundingBoxes(boxes: Collection<Rect>, joinDistanceX : Int = 5, joinDistanceY: Int = 10) : Collection<Rect> {
         val processed = arrayOfNulls<Boolean>(boxes.size)
         val newBoxes = mutableListOf<Rect>()
 
         boxes.forEachIndexed { i, r -> {
-
+        // TODO
         } }
 
         return newBoxes
     }
 
-    private fun getContours(frame: Mat, prediction: Rect): Collection<Rect> {
+    private fun getContours(frame: Mat): Collection<Rect> {
         val edges = Mat()
         Imgproc.Canny(frame, edges, 100.0, 200.0)
         val contours = mutableListOf<MatOfPoint>()
         Imgproc.findContours(edges, contours, null, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE) // FIXME: or empty mat for hierarchy?
-        return contours.map { Imgproc.boundingRect(it) }
+        val boundingBoxes = contours.map { Imgproc.boundingRect(it) }
+        return joinNearbyBoundingBoxes(boundingBoxes)
     }
 }
